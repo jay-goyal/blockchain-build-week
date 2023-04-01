966d9fcba32da20ea6cb7374ae653743d898db89
rishee <rishee.rb@gmail.com>
2023-04-01 12:22:04 +0530

    About us

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./index.css";
 import Timeline from "./components/Timeline/Timeline";
-import Lander from "./Components/Lander"
+import Lander from "./Components/Lander";
+import AboutUs from "./components/About us/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Lander />
-      {/* <Timeline /> */}
+      <AboutUs />
+      <Timeline />
     </div>
   );
 }
---
+++ src/Components/About us/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="container">
+      <div className="dome">
+        <h2> About us</h2>
+        <p className="about-us-description">
+          We are a group of blockchain enthusiasts that have banded together to
+          form a community of developers, innovators, and creators that strive
+          to better themselves every day and have fun while building cool stuff
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
