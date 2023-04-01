923e5a981717651f1ca9321a526815f94d69db41
Jay Goyal <goyal.jay2003@gmail.com>
2023-04-01 11:26:49 +0530

    Update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.css";
 import Timeline from "./components/Timeline/Timeline";
+import Lander from "./Components/Lander"
 
 function App() {
   return (
     <div className="App">
-      <Timeline />
       <Lander />
+      {/* <Timeline /> */}
     </div>
   );
 }
