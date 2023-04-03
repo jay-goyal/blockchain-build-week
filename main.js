2c0cec005c0c30688072c2393617fbc54cbcd3a9
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-04-04 03:26:39 +0530

    lets win the css design awards: part 1

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-import './style.css'
+import Lenis from "@studio-freight/lenis"
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+new kursor({
+    type: 5,
+    color: "#ffffff",
+})
+
+const lenis = new Lenis({
+    smoothTouch: true,
+    smooth: true,
+    smoothWheel: true,
+})
+
+window.lenis = lenis
+
+const scroller = document.getElementById("scroller")
+scroller.style.width = "0%"
+
+lenis.on("scroll", (data) => {
+    const maxScroll = data.dimensions.scrollHeight - data.dimensions.height
+    const scroll = data.animatedScroll
+    scroller.style.width = `${(scroll * 100) / maxScroll}%`
+
+    // console.log(data)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
