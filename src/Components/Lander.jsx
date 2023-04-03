ee58bdb74977260a09d516dd21cb5c89a3657141
Jay Goyal <goyal.jay2003@gmail.com>
2023-04-03 16:17:08 +0530

    Lander tweaks

--- src/Components/Lander.jsx
+++ src/Components/Lander.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "../CSS/Lander.css";
-import { useState } from "react";
-import Countdown from "./Countdown";
 
 export default function Lander() {
-  const [isCount, setIsCount] = useState(false);
-
   return (
     <div className="LanderCont">
       {/* <img src="/globe.png" alt="globe" className="LanderGlobe" /> */}
       <img src="/logo.svg" alt="BITS Blockchain Logo" className="LanderLogo" />
       <h1 className="LanderTitle">build week</h1>
-        <a href="https://forms.gle/hmxdeMrsKdgSeQHY9" className="LanderBtn">
-          START BUILDING
-        </a>
+      <a href="https://forms.gle/hmxdeMrsKdgSeQHY9" className="LanderBtn">
+        START BUILDING
+      </a>
       <img src="/ringLeft.png" alt="bg ring" className="LanderRing ringLeft" />
       <img
         src="/ringRight.png"
         alt="bg ring"
         className="LanderRing ringRight"
       />
+      <img src="/globe.svg" alt="globe" className="LanderGlobe" />
       <div className="LanderFade" />
     </div>
   );
