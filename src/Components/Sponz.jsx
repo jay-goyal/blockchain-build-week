215e92090aa25f272222656b60d341a51b9ebca4
Sarthak Aggarwal <getsarthakaggarwal@gmail.com>
2023-04-05 00:25:24 +0530

    added sponsors

--- src/Components/Sponz.jsx
+++ src/Components/Sponz.jsx
@@ -6,46 +6,74 @@ export default function Sponz() {
     <div className="spons-container">
       <div className="spons-dome">
         <h2>Sponsors</h2>
-        <h3>Official hosting Partner</h3>
+        <h3>Hosting Partner</h3>
 
         <div className="sponsor">
           <img
-            src="https://cdn.logojoy.com/wp-content/uploads/2018/08/23161109/2_big-2.png"
+            src="https://camo.githubusercontent.com/4079196b56fceacd94ff768d51a1b9b8f5ec11f31ab9a76d417be4b128e3c0cd/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f6c65746563682d6469676974616c2d736f6c7574696f6e732f696d6167652f75706c6f61642f76313637343535343437332f47726f75705f3133315f6d636d6e32652e706e67"
             alt="our sponsor"
           />
-          <h4>Spheron</h4>
         </div>
 
-        <h3>Official hosting Partner</h3>
+        <h3>Web3 Cross Chain Sponsor</h3>
 
         <div className="sponsor">
-          <img
-            src="https://cdn.logojoy.com/wp-content/uploads/2018/08/23161109/2_big-2.png"
-            alt="our sponsor"
-          />
-          <h4>Spheron</h4>
-        </div>
+      <img
+    src="https://www.cryptoninjas.net/wp-content/uploads/router-protocol-crypto-ninjas.jpg"
+    alt="our sponsor"
+  />
+</div>
 
-        <h3>Official hosting Partner</h3>
 
-        <div className="sponsor">
-          <img
-            src="https://cdn.logojoy.com/wp-content/uploads/2018/08/23161109/2_big-2.png"
-            alt="our sponsor"
-          />
-          <h4>Spheron</h4>
-        </div>
+<h3>Metaverse Partner</h3>
 
-        <h3>Official hosting Partner</h3>
+<div className="sponsor">
+<img
+src="https://www.gamestate.one/static/media/logoFab.d7b5b976.png"
+alt="our sponsor"
+/>
+</div>
 
-        <div className="sponsor">
-          <img
-            src="https://cdn.logojoy.com/wp-content/uploads/2018/08/23161109/2_big-2.png"
-            alt="our sponsor"
-          />
-          <h4>Spheron</h4>
-        </div>
-      </div>
+
+
+<h3>Hackathon Partner</h3>
+
+<div className="devfolio">
+<img
+src="https://cdn.iconscout.com/icon/free/png-256/devfolio-1649779-1399882.png"
+alt="our sponsor"
+/>
+</div>
+
+<h3>Track Partners</h3>
+<div className="track-partners">
+<div className="track-sponsor">
+<img
+src="https://www.forbes.com/advisor/wp-content/uploads/2022/08/Polygon_logo_resized.jpeg.jpg"
+alt="our sponsor"
+/>
+</div>
+
+<div className="track-sponsor">
+<img
+src="https://solana.com/src/img/branding/solanaVerticalLogo.png"
+alt="our sponsor"
+/>
+</div>
+
+<div className="track-sponsor">
+<img
+src="https://cdn.brandfolder.io/CBWQP80B/as/85hkgbtjbbhstpqn4sxssgx/450x250-partnerlogo-filecoin.png"
+alt="our sponsor"
+/>
+</div>
+
+</div>
+
+
+
+       
+     </div>
     </div>
   );
 }
