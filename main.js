1b0c7716cb1f6d8e1de9c429b4ab06fd8cb1c95e
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-04-04 06:07:33 +0530

    optimizing js framerate for mobile

--- main.js
+++ main.js
@@ -12,9 +12,16 @@ const cursorBox = document.getElementById("cursorBox")
 const cursor = document.getElementById("cursor")
 scroller.style.width = "0%"
 
-var button = document.getElementById("debounce")
+const isTouchDevice = "ontouchstart" in document.documentElement
+
+if (isTouchDevice) {
+  cursor.style.display = "hidden"
+  cursorBox.style.display = "hidden"
+}
+
 const debounce = (func, delay) => {
     let debounceTimer
+    if (isTouchDevice) return function () {}
     return function () {
         const context = this
         const args = arguments
@@ -23,38 +30,40 @@ const debounce = (func, delay) => {
     }
 }
 
-document.body.onpointermove = debounce((event) => {
-    const { clientX, clientY } = event
-    cursor.style.left = `${clientX}px`
-    cursor.style.top = `${clientY}px`
-    cursorBox.animate(
-        {
-            left: `${clientX}px`,
-            top: `${clientY}px`,
-        },
-        { duration: 1000, fill: "forwards" }
-    )
-}, 10)
+{
+    document.body.onpointermove = debounce((event) => {
+        const { clientX, clientY } = event
+        cursor.style.left = `${clientX}px`
+        cursor.style.top = `${clientY}px`
+        cursorBox.animate(
+            {
+                left: `${clientX}px`,
+                top: `${clientY}px`,
+            },
+            { duration: 1000, fill: "forwards" }
+        )
+    }, 10)
 
-document.body.onpointerdown = debounce((_) => {
-    cursorBox.animate(
-        {
-            width: "24px",
-            height: "24px",
-        },
-        { duration: 100, fill: "forwards" }
-    )
-}, 50)
+    document.body.onpointerdown = debounce((_) => {
+        cursorBox.animate(
+            {
+                width: "24px",
+                height: "24px",
+            },
+            { duration: 100, fill: "forwards" }
+        )
+    }, 50)
 
-document.body.onpointerup = debounce((_) => {
-    cursorBox.animate(
-        {
-            width: "32px",
-            height: "32px",
-        },
-        { duration: 100, fill: "forwards" }
-    )
-}, 50)
+    document.body.onpointerup = debounce((_) => {
+        cursorBox.animate(
+            {
+                width: "32px",
+                height: "32px",
+            },
+            { duration: 100, fill: "forwards" }
+        )
+    }, 50)
+}
 
 lenis.on("scroll", (data) => {
     const maxScroll = data.dimensions.scrollHeight - data.dimensions.height
